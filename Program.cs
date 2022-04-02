b2fbfb0f9966a5745fc469808cbde2d966e1ae57
Marcello Bonadias <marcelloebonadias@gmail.com>
2022-04-01 22:34:23 -0300

    Finalizar Tarefa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double numerador, denominador, quociente;
+
+Console.Write("Digite o numerador......: "); 
+    numerador = Convert.ToDouble(Console.ReadLine()); 
+
+Console.WriteLine("Digite o denominador......: ");
+    denominador = Convert.ToDouble(Console.ReadLine()); 
+
+    if (numerador == 0)
+    {
+        Console.WriteLine ("Não pode ser dividido por zero.");
+    }
+
+    else 
+    {
+    quociente = numerador / denominador;
+
+    Console.WriteLine($"{numerador:N1} dividido por {denominador:N1} é {quociente:N1}.");
+    }
